435573b42fa8b3dac7610a4448cb9e059e7c9dbd
HylaDev <ernestdossa.9@gmail.com>
2024-11-12 16:32:56 +0100

    register route

--- api/routes/users/routes.js
+++ api/routes/users/routes.js
@@ -1,9 +1,30 @@
-import data from '../../../data'
+import userExists from '../../services/checkUser/index.js';
 import express from 'express';
 
 const router = express.Router();
+router.get("/", (req, res) => {
+    res.send({ message: "users" });
+    });
 
-router.post("/sign-up", async (req, res) => {
+
+router.post("register", async (req, res) => {
+    
+    const {nom, email, password} = req.body;
+    console.log(req.body);
+    
+    /*try {
+        if (userExists(email)) {
+            console.log("Le user existe");
+            res.status(400).json({ message: "L'utilisateur existe déjà" });
+          } else {
+            console.log("Le user n'existe pas");
+            res.status(200).json({ message: "Utilisateur créé" });
+          }
+    }
+    catch (error) {
+        console.log(error)
+        return res.status(500).json({ message: "Erreur de serveur" });
+      }*/
 
 }); 
  export default router;
--- server.js
+++ server.js
@@ -1,16 +1,19 @@
 import express from 'express';
-import usersRoutes from './api/routes/users/routes.js'
+import usersRoutes from './api/routes/users/routes.js';
+import bodyParser from 'body-parser';
 const app = express();
 
 app.use(express.json());
+app.use(bodyParser.json());
 
 
 app.get("/", (req, res) => {
 res.send({ message: "Welcome to your bank account management app" });
 });
 
-app.use("users", usersRoutes);
+app.use("/users", usersRoutes);
+
 
 
 const port = 3000;
-const server = app.listen(port, () => console.log(`listening on port ${port}`));
+app.listen(port, () => console.log(`listening on port ${port}`));
