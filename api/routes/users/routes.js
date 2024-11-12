c502b29fd2a77c6f06b55514f911c76182266af4
HylaDev <ernestdossa.9@gmail.com>
2024-11-12 15:14:12 +0100

    create route

---
+++ api/routes/users/routes.js
@@ -0,0 +1,9 @@
+import data from '../../../data'
+import express from 'express';
+
+const router = express.Router();
+
+router.post("/sign-up", async (req, res) => {
+
+}); 
+ export default router;
--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 import express from 'express';
+import usersRoutes from './api/routes/users/routes.js'
 const app = express();
 
 app.use(express.json());
 
 
 app.get("/", (req, res) => {
-res.send({ message: "Welcome to monitoring web app" });
+res.send({ message: "Welcome to your bank account management app" });
 });
+
+app.use("users", usersRoutes);
 
 
 const port = 3000;
