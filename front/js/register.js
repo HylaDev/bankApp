effe20d78c11711433bf21c33b75dc024afde139
jennifer <comlanvihilary@gmail.com>
2024-11-13 12:32:27 +0100

    inscription

---
+++ front/js/register.js
@@ -0,0 +1,60 @@
+const API_URL = 'http://localhost:3000';
+
+$(document).ready(function() {
+    $('#signupForm').on('submit', function(e){
+        e.preventDefault();
+        const name = $('#name').val().trim();
+        const email = $('#email').val().trim();
+        const pass = $('#password').val().trim();
+        const confirm  = $('#confirm-password').val().trim();
+
+        $('.error-message').text('').hide();
+        let hasError = false;
+
+        if(name === '') {
+            $('#name-error').text('Veuillez entrer votre nom complet').show();
+            hasError = true;
+        }
+        if(!validateEmail(email)) {
+            $('#email-error').text('Veuillez entrer une adresse email valide').show();
+            hasError = true;
+        }
+
+        if (pass.length < 8) {
+            $('#pass-error').text("Le mot de passe doit contenir au moins 8 caractères.").show();
+            hasError = true;
+        }
+
+        if (pass !== confirm) {
+            $('#confirm-error').text("Les mots de passe ne correspondent pas.").show();
+            hasError = true;
+        }
+
+        if (hasError) return;
+        $.ajax({
+            url: `${API_URL}/users/register`,
+            method: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify({
+                name: name,
+                email: email,
+                pass: pass
+              }),
+            success: function(response) {
+                console.log(response);
+                $('#signupForm').trigger('reset');
+                // $('#success-message').text('Votre inscription a bien été prise en compte! Vous pouvez vous connecter maintenant.').show();
+                alert('Inscription reusi')
+            },
+            error: function(error) {
+                console.error(error);
+                // $('#error-message').text('Une erreur est survenue lors de l\'inscription. Veuillez réessayer.').show();
+            }
+        })
+    })
+
+    function validateEmail(email) {
+        const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return emailPattern.test(email);
+      }
+});
