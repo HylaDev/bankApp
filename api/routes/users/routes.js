92be8bf83d9e001bdf3b981587f9cc9537b3e266
HylaDev <ernestdossa.9@gmail.com>
2024-11-14 11:03:16 +0100

    get balance after transaction and translate message to french

--- api/routes/users/routes.js
+++ api/routes/users/routes.js
@@ -6,7 +6,6 @@ import { fileURLToPath } from 'url';
 import path from 'path';
 import generateJwt from '../../services/jwt/index.js';
 import isAuthenticated from '../../../middlewares/isAuth/index.js';
-import {checkAccountBalance} from '../../services/checkAccounts/index.js';
 
 
 const __filename = fileURLToPath(import.meta.url);
@@ -47,7 +46,7 @@ router.get("/profile", isAuthenticated, async (req, res) => {
     const userExists = users.some((user) => user.email === email);
 
     if (userExists) {
-        return res.status(409).json({ message: 'User already exists.' });
+        return res.status(409).json({ message: 'Cet utilisateur existe déjà.' });
     }
     const password = await hashPassword(pass);
 
@@ -63,7 +62,7 @@ router.get("/profile", isAuthenticated, async (req, res) => {
     const userToken = await generateJwt(payload);
     res.cookie("auth_token", userToken, { httpOnly: true});
 
-    res.status(201).json({ message: 'User registered and login successfully.', user: newUser.email, userToken: userToken});
+    res.status(201).json({ message: 'Inscription avec succès .', user: newUser.email, userToken: userToken});
 });
 
   // login user
@@ -90,7 +89,7 @@ router.get("/profile", isAuthenticated, async (req, res) => {
       };
       const userToken = await generateJwt(payload);
       res.cookie("auth_token", userToken, { httpOnly: true});
-      return res.status(200).json({message:"user login", userToken:userToken, userEmail:user.email, userName:user.name})
+      return res.status(200).json({message:"Vous êtes bien connecté", userToken:userToken, userEmail:user.email, userName:user.name})
     }
     return res.status(409).json({message:"connexion echouée"})
 
@@ -101,7 +100,7 @@ router.get("/profile", isAuthenticated, async (req, res) => {
     // Effacez le cookie contenant le jeton JWT
     res.clearCookie("auth_token", { httpOnly: true, secure: true });
     
-    res.status(200).json({ message: "Logout successful" });
+    res.status(200).json({ message: "Déconnexion avec succès" });
   });
 
 
@@ -115,7 +114,7 @@ router.get("/profile", isAuthenticated, async (req, res) => {
     
     const parsedInitialB = parseFloat(initialBalance);
     if (isNaN(parsedInitialB) || parsedInitialB <= 0) {
-        return res.status(400).json({ message: "Invalid amount. Amount must be a positive number." });
+        return res.status(400).json({ message: "Montant invalide, il doit être supérieur à zéro." });
     }
 
     const users = readData();
@@ -129,7 +128,7 @@ router.get("/profile", isAuthenticated, async (req, res) => {
     }
     const account = user.accounts.find((acc) => acc.accountType == accountType);
     if(account){
-      return res.status(404).json({ message: 'This account type already exist.' });
+      return res.status(404).json({ message: 'Ce compte type existe déjà.' });
     }
     const newAccount = {
         accountNumber: `YEJ-${Date.now()}`,
@@ -141,7 +140,7 @@ router.get("/profile", isAuthenticated, async (req, res) => {
     user.accounts.push(newAccount);
     saveData(users);
 
-    res.status(201).json({ message: 'Bank account created successfully.', account: newAccount });
+    res.status(201).json({ message: 'Compte bancaire créé avec succès.', account: newAccount });
   });
 
   // list user accounts
@@ -160,16 +159,16 @@ router.get("/profile", isAuthenticated, async (req, res) => {
 
   // add transaction to user
   router.post("/add/transaction", isAuthenticated, async (req, res) =>{
-    const {email, accountType, transactionType, amount, date} = req.body;
+    const {email, accountType, transactionType, amount} = req.body;
 
 
-    if (!email || !transactionType || !amount || !date){
+    if (!email || !transactionType || !amount){
       res.status(400).json({message: "email, transactionType, amount and date are required"});
     }
 
     const parsedAmount = parseFloat(amount);
     if (isNaN(parsedAmount) || parsedAmount <= 0) {
-        return res.status(400).json({ message: "Invalid amount. Amount must be a positive number." });
+        return res.status(400).json({ message: "Montant invalide, il doit être supérieur à zéro." });
     }
 
     const users = readData();
@@ -178,44 +177,43 @@ router.get("/profile", isAuthenticated, async (req, res) => {
       return res.status(404).json({message:"user not found"});
     }
     if (!user.accounts){
-      res.status(404).json({message:"user doesn't have account"})
+      res.status(404).json({message:"Vous n'avez pas de compte"})
     }
 
     const account = user.accounts.find((acc) => acc.accountType == accountType);
-    console.log(account)
     if(!account){
-      res.status(404).json({message:"user doesn't have this account type"})
+      res.status(404).json({message:"Vous n'avez pas ce type de compte"})
     }
 
     if (!account.transactions) {
       account.transactions = [];
     }
-
+    let newBalance = account.balance;
     if (transactionType === "Retrait") {
-        if (account.balance < parsedAmount) {
-            return res.status(400).json({ message: "Insufficient balance" });
+        if (newBalance < parsedAmount) {
+            return res.status(400).json({ message: "Votre solde est insufisant" });
         }
-        account.balance -= parsedAmount;
+        newBalance -= parsedAmount;
     } else if (transactionType === "Depot") {
-        account.balance += parsedAmount;
+      newBalance += parsedAmount;
     } else {
-        return res.status(400).json({ message: "Invalid transaction type" });
+        return res.status(400).json({ message: "Ce type de transaction n'existe pas" });
     }
-    
-    
+
     const userTransaction = {
       transactionType, 
       amount: parsedAmount,
-      date
+      date: new Date(),
+      newBalance: newBalance
     }
     account.transactions.push(userTransaction);
-
+    account.balance = newBalance;
     saveData(users);
-    if(account.balance < account.threshold){
-      res.status(200).json({message:"transaction added", transaction: userTransaction, notification: "Alerte : Le solde est en dessous du seuil défini!" })
+    if(newBalance < account.threshold){
+      res.status(200).json({message:"Tansaction effectuée", transaction: userTransaction, notification: "Alerte : Le solde est en dessous du seuil défini!" })
 
     }
-    res.status(200).json({message:"transaction added", transaction: userTransaction })
+    res.status(200).json({message:"Tansaction effectuée", transaction: userTransaction })
 
   })
 
@@ -231,7 +229,7 @@ router.get("/profile", isAuthenticated, async (req, res) => {
       }
       const account = user.accounts.find((acc) => acc.accountType == accountType)
       if(!account){
-        res.status(404).json({message:"user doesn't have this account type"})
+        res.status(404).json({message:"Vous n'avez pas ce type de compte"})
       }
   
       if (!account.transactions) {
@@ -239,7 +237,7 @@ router.get("/profile", isAuthenticated, async (req, res) => {
       }
 
       if (!account.transactions || !Array.isArray(account.transactions)) {
-          return res.status(404).json({ message: "User doesn't have any transactions" });
+          return res.status(404).json({ message: "Vous n'avez aucune transaction" });
       }
       res.status(200).json({transaction: account.transactions})
 
@@ -254,14 +252,14 @@ router.get("/profile", isAuthenticated, async (req, res) => {
   
     const account = user.accounts.find((acc) => acc.accountType == accountType)
     if(!account){
-      res.status(404).json({message:"user doesn't have this account type"})
+      res.status(404).json({message:"Vous n'avez pas ce type de compte"})
     }
   
     account.threshold = threshold;
     saveData(users);
   
     res.status(200).json({
-      message: 'threshold updated successfuly',
+      message: 'Seuil mis à jour avec succès',
       threshold: threshold
     });
   });
@@ -278,7 +276,7 @@ router.get("/profile", isAuthenticated, async (req, res) => {
     }
 
     if (!user.accounts || !Array.isArray(user.accounts)) {
-        return res.status(404).json({ message: "User doesn't have any accounts" });
+        return res.status(404).json({ message: "Vous n'avez pas de compte" });
     }
 
     const totalBalance = user.accounts.reduce((sum, account) => sum + account.balance, 0);
